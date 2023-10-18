c5adbb1b75b227570b18f5c1e7d5f627cdf33006
bryan967132 <bryan.tejaxun@gmail.com>
2023-10-18 06:00:10 -0600

    C3D

--- src/BackEnd/Classes/Expressions/Primitive.go
+++ src/BackEnd/Classes/Expressions/Primitive.go
@@ -4,6 +4,9 @@ import (
 	env "TSwift/Classes/Env"
 	C3DGen "TSwift/Classes/Generator"
 	utils "TSwift/Classes/Utils"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Primitive struct {
@@ -29,10 +32,38 @@ func (pr *Primitive) ColumnN() int {
 func (pr *Primitive) Exec(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
 	switch pr.Type {
 	case utils.INT:
+		intValue, _ := strconv.Atoi(pr.Value.(string))
+		return &utils.ReturnValue{StrValue: fmt.Sprintf("%v", pr.Value), IntValue: intValue, IsTmp: false, Type: pr.Type}
 	case utils.FLOAT:
+		return &utils.ReturnValue{StrValue: fmt.Sprintf("%v", pr.Value), IsTmp: false, Type: pr.Type}
 	case utils.BOOLEAN:
+		c3dgen.AddComment("------ Boolean Primitive ------")
+		trueLbl := c3dgen.NewLabel()
+		falseLbl := c3dgen.NewLabel()
+		if pr.Value.(string) == "true" {
+			c3dgen.AddGoto(trueLbl)
+		} else {
+			c3dgen.AddGoto(falseLbl)
+		}
+		return &utils.ReturnValue{IsTmp: false, Type: pr.Type, TrueLabel: []string{trueLbl}, FalseLabel: []string{falseLbl}}
 	case utils.NIL:
+		return &utils.ReturnValue{IsTmp: false, Type: pr.Type}
 	default:
+		pr.Value = strings.ReplaceAll(fmt.Sprintf("%v", pr.Value), "\\n", "\n")
+		pr.Value = strings.ReplaceAll(fmt.Sprintf("%v", pr.Value), "\\t", "\t")
+		pr.Value = strings.ReplaceAll(fmt.Sprintf("%v", pr.Value), "\\r", "\r")
+		pr.Value = strings.ReplaceAll(fmt.Sprintf("%v", pr.Value), "\\\"", "\"")
+		pr.Value = strings.ReplaceAll(fmt.Sprintf("%v", pr.Value), "\\'", "'")
+		pr.Value = strings.ReplaceAll(fmt.Sprintf("%v", pr.Value), "\\\\", "\\")
+		c3dgen.AddComment("------ String Primitive ------")
+		newTemp := c3dgen.NewTemp()
+		c3dgen.AddAssign(newTemp, "H")
+		for _, asc := range []byte(pr.Value.(string)) {
+			c3dgen.AddSetHeap("(int) H", strconv.Itoa(int(asc)))
+			c3dgen.AddExpression("H", "H", "+", "1")
+		}
+		c3dgen.AddSetHeap("(int) H", "-1")
+		c3dgen.AddExpression("H", "H", "+", "1")
+		return &utils.ReturnValue{StrValue: newTemp, IsTmp: true, Type: pr.Type}
 	}
-	return nil
 }
--- src/BackEnd/Classes/Instructions/Print.go
+++ src/BackEnd/Classes/Instructions/Print.go
@@ -5,6 +5,8 @@ import (
 	C3DGen "TSwift/Classes/Generator"
 	interfaces "TSwift/Classes/Interfaces"
 	utils "TSwift/Classes/Utils"
+	"fmt"
+	"strconv"
 )
 
 type Print struct {
@@ -27,5 +29,49 @@ func (prt *Print) ColumnN() int {
 }
 
 func (prt *Print) Exec(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
+	if prt.Exps != nil {
+		for _, exp := range prt.Exps {
+			value1 := exp.Exec(env, c3dgen)
+			if value1.Type == utils.INT {
+				c3dgen.AddComment("------ Print Int ------")
+				c3dgen.AddPrintf("d", fmt.Sprintf("(int) %s", value1.StrValue))
+			} else if value1.Type == utils.FLOAT {
+				c3dgen.AddComment("------ Print Float ------")
+				c3dgen.AddPrintf("f", value1.StrValue)
+			} else if value1.Type == utils.BOOLEAN {
+				c3dgen.AddComment("------ Print Boolean ------")
+				newLabel := c3dgen.NewLabel()
+				for _, lbl := range value1.TrueLabel {
+					c3dgen.AddLabel(lbl)
+				}
+				c3dgen.AddComment("------ Print true ------")
+				c3dgen.AddPrint("true")
+				c3dgen.AddGoto(newLabel)
+				for _, lbl := range value1.FalseLabel {
+					c3dgen.AddLabel(lbl)
+				}
+				c3dgen.AddComment("------ Print false ------")
+				c3dgen.AddPrint("false")
+				c3dgen.AddLabel(newLabel)
+			} else if value1.Type == utils.STRING {
+				c3dgen.GeneratePrintString()
+				newTemp1 := c3dgen.NewTemp()
+				newTemp2 := c3dgen.NewTemp()
+				size := strconv.Itoa((*env.Size)["size"])
+				c3dgen.AddComment("------ Print String ------")
+				c3dgen.AddExpression(newTemp1, "P", "+", size)
+				c3dgen.AddExpression(newTemp1, newTemp1, "+", "1")
+				c3dgen.AddSetStack("(int) "+newTemp1, value1.StrValue)
+				c3dgen.AddExpression("P", "P", "+", size)
+				c3dgen.AddCall("printString")
+				c3dgen.AddGetStack(newTemp2, "(int) P")
+				c3dgen.AddExpression("P", "P", "-", size)
+			} else {
+				c3dgen.AddComment("------ Print nil ------")
+				c3dgen.AddPrint("nil")
+			}
+		}
+	}
+	c3dgen.AddPrint("\n")
 	return nil
 }
