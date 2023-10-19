4a1971a06e7f3dc6d8368d6e796101f44b59c980
bryan967132 <bryan.tejaxun@gmail.com>
2023-10-19 02:03:44 -0600

    C3D

--- src/BackEnd/Classes/Expressions/Relational.go
+++ src/BackEnd/Classes/Expressions/Relational.go
@@ -29,5 +29,124 @@ func (rl *Relational) ColumnN() int {
 }
 
 func (rl *Relational) Exec(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
-	return nil
+	switch rl.Sign {
+	case "==":
+		return rl.equal(env, c3dgen)
+	case "!=":
+		return rl.notEqual(env, c3dgen)
+	case ">=":
+		return rl.moreEqual(env, c3dgen)
+	case "<=":
+		return rl.lessEqual(env, c3dgen)
+	case ">":
+		return rl.more(env, c3dgen)
+	case "<":
+		return rl.less(env, c3dgen)
+	default:
+		return &utils.ReturnValue{IsTmp: false, Type: utils.NIL}
+	}
+}
+
+func (rl *Relational) equal(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
+	value1 := rl.Exp1.Exec(env, c3dgen)
+	value2 := rl.Exp2.Exec(env, c3dgen)
+	if value1.Type == utils.INT && value2.Type == utils.INT ||
+		value1.Type == utils.FLOAT && value2.Type == utils.FLOAT ||
+		value1.Type == utils.BOOLEAN && value2.Type == utils.BOOLEAN ||
+		value1.Type == utils.STRING && value2.Type == utils.STRING ||
+		value1.Type == utils.CHAR && value2.Type == utils.CHAR {
+		trueLbl := c3dgen.NewLabel()
+		falseLbl := c3dgen.NewLabel()
+		c3dgen.AddIf(value1.StrValue, value2.StrValue, "==", trueLbl)
+		c3dgen.AddGoto(falseLbl)
+		return &utils.ReturnValue{IsTmp: false, Type: utils.BOOLEAN, TrueLabel: []string{trueLbl}, FalseLabel: []string{falseLbl}}
+	}
+	env.SetError("Los tipos no son válidos para operaciones relacionales", rl.Exp1.LineN(), rl.Exp1.ColumnN())
+	return &utils.ReturnValue{IsTmp: false, Type: utils.NIL}
+}
+
+func (rl *Relational) notEqual(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
+	value1 := rl.Exp1.Exec(env, c3dgen)
+	value2 := rl.Exp2.Exec(env, c3dgen)
+	if value1.Type == utils.INT && value2.Type == utils.INT ||
+		value1.Type == utils.FLOAT && value2.Type == utils.FLOAT ||
+		value1.Type == utils.BOOLEAN && value2.Type == utils.BOOLEAN ||
+		value1.Type == utils.STRING && value2.Type == utils.STRING ||
+		value1.Type == utils.CHAR && value2.Type == utils.CHAR {
+		trueLbl := c3dgen.NewLabel()
+		falseLbl := c3dgen.NewLabel()
+		c3dgen.AddIf(value1.StrValue, value2.StrValue, "!=", trueLbl)
+		c3dgen.AddGoto(falseLbl)
+		return &utils.ReturnValue{IsTmp: false, Type: utils.BOOLEAN, TrueLabel: []string{trueLbl}, FalseLabel: []string{falseLbl}}
+	}
+	env.SetError("Los tipos no son válidos para operaciones relacionales", rl.Exp1.LineN(), rl.Exp1.ColumnN())
+	return &utils.ReturnValue{IsTmp: false, Type: utils.NIL}
+}
+
+func (rl *Relational) moreEqual(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
+	value1 := rl.Exp1.Exec(env, c3dgen)
+	value2 := rl.Exp2.Exec(env, c3dgen)
+	if value1.Type == utils.INT && value2.Type == utils.INT ||
+		value1.Type == utils.FLOAT && value2.Type == utils.FLOAT ||
+		value1.Type == utils.STRING && value2.Type == utils.STRING ||
+		value1.Type == utils.CHAR && value2.Type == utils.CHAR {
+		trueLbl := c3dgen.NewLabel()
+		falseLbl := c3dgen.NewLabel()
+		c3dgen.AddIf(value1.StrValue, value2.StrValue, ">=", trueLbl)
+		c3dgen.AddGoto(falseLbl)
+		return &utils.ReturnValue{IsTmp: false, Type: utils.BOOLEAN, TrueLabel: []string{trueLbl}, FalseLabel: []string{falseLbl}}
+	}
+	env.SetError("Los tipos no son válidos para operaciones relacionales", rl.Exp1.LineN(), rl.Exp1.ColumnN())
+	return &utils.ReturnValue{IsTmp: false, Type: utils.NIL}
+}
+
+func (rl *Relational) lessEqual(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
+	value1 := rl.Exp1.Exec(env, c3dgen)
+	value2 := rl.Exp2.Exec(env, c3dgen)
+	if value1.Type == utils.INT && value2.Type == utils.INT ||
+		value1.Type == utils.FLOAT && value2.Type == utils.FLOAT ||
+		value1.Type == utils.STRING && value2.Type == utils.STRING ||
+		value1.Type == utils.CHAR && value2.Type == utils.CHAR {
+		trueLbl := c3dgen.NewLabel()
+		falseLbl := c3dgen.NewLabel()
+		c3dgen.AddIf(value1.StrValue, value2.StrValue, "<=", trueLbl)
+		c3dgen.AddGoto(falseLbl)
+		return &utils.ReturnValue{IsTmp: false, Type: utils.BOOLEAN, TrueLabel: []string{trueLbl}, FalseLabel: []string{falseLbl}}
+	}
+	env.SetError("Los tipos no son válidos para operaciones relacionales", rl.Exp1.LineN(), rl.Exp1.ColumnN())
+	return &utils.ReturnValue{IsTmp: false, Type: utils.NIL}
+}
+
+func (rl *Relational) more(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
+	value1 := rl.Exp1.Exec(env, c3dgen)
+	value2 := rl.Exp2.Exec(env, c3dgen)
+	if value1.Type == utils.INT && value2.Type == utils.INT ||
+		value1.Type == utils.FLOAT && value2.Type == utils.FLOAT ||
+		value1.Type == utils.STRING && value2.Type == utils.STRING ||
+		value1.Type == utils.CHAR && value2.Type == utils.CHAR {
+		trueLbl := c3dgen.NewLabel()
+		falseLbl := c3dgen.NewLabel()
+		c3dgen.AddIf(value1.StrValue, value2.StrValue, ">", trueLbl)
+		c3dgen.AddGoto(falseLbl)
+		return &utils.ReturnValue{IsTmp: false, Type: utils.BOOLEAN, TrueLabel: []string{trueLbl}, FalseLabel: []string{falseLbl}}
+	}
+	env.SetError("Los tipos no son válidos para operaciones relacionales", rl.Exp1.LineN(), rl.Exp1.ColumnN())
+	return &utils.ReturnValue{IsTmp: false, Type: utils.NIL}
+}
+
+func (rl *Relational) less(env *env.Env, c3dgen *C3DGen.C3DGen) *utils.ReturnValue {
+	value1 := rl.Exp1.Exec(env, c3dgen)
+	value2 := rl.Exp2.Exec(env, c3dgen)
+	if value1.Type == utils.INT && value2.Type == utils.INT ||
+		value1.Type == utils.FLOAT && value2.Type == utils.FLOAT ||
+		value1.Type == utils.STRING && value2.Type == utils.STRING ||
+		value1.Type == utils.CHAR && value2.Type == utils.CHAR {
+		trueLbl := c3dgen.NewLabel()
+		falseLbl := c3dgen.NewLabel()
+		c3dgen.AddIf(value1.StrValue, value2.StrValue, "<", trueLbl)
+		c3dgen.AddGoto(falseLbl)
+		return &utils.ReturnValue{IsTmp: false, Type: utils.BOOLEAN, TrueLabel: []string{trueLbl}, FalseLabel: []string{falseLbl}}
+	}
+	env.SetError("Los tipos no son válidos para operaciones relacionales", rl.Exp1.LineN(), rl.Exp1.ColumnN())
+	return &utils.ReturnValue{IsTmp: false, Type: utils.NIL}
 }
