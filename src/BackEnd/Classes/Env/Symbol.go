a7f0d45f4757315bcbdb36d8644603e82a3c7b72
bryan967132 <bryan.tejaxun@gmail.com>
2023-10-20 05:29:36 -0600

    Symbol

--- src/BackEnd/Classes/Env/Symbol.go
+++ src/BackEnd/Classes/Env/Symbol.go
@@ -5,12 +5,12 @@ import utils "TSwift/Classes/Utils"
 type Symbol struct {
 	IsVariable  bool
 	IsPrimitive bool
-	Value       interface{}
 	Id          string
 	Type        utils.Type
 	ArrType     utils.Type
+	Position    int
 }
 
-func NewSymbol(isVariable bool, isPrimitive bool, value interface{}, id string, Type, arrType utils.Type) *Symbol {
-	return &Symbol{isVariable, isPrimitive, value, id, Type, arrType}
+func NewSymbol(isVariable bool, isPrimitive bool, id string, Type, arrType utils.Type, position int) *Symbol {
+	return &Symbol{isVariable, isPrimitive, id, Type, arrType, position}
 }
